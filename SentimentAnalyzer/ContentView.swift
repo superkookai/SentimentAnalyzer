3413f89d2173673875bb6df9ecf333e6da0fb829
superkookai <weerawut@live.com>
2025-10-18 09:38:41 +0700

    Initial Commit

---
+++ SentimentAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SentimentAnalyzer
+//
+//  Created by Weerawut Chaiyasomboon on 18/10/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SentimentAnalyzer/SentimentAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalyzerApp.swift
+//  SentimentAnalyzer
+//
+//  Created by Weerawut Chaiyasomboon on 18/10/2568.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
